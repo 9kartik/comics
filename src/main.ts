26d28d3c6eed34ef3c94c66edbee3e9d3a7e4413
9kartik <mauryakartik@gmail.com>
2024-09-17 01:54:22 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="slider">
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Cover.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Chargeable.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Child_power.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Compliment.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Drivers_license.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Ex_wedding.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Lizard.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Marriage.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Nails.png" alt="Rock Dove Comic"/>
+    </section>
+    <section>  
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Rock_pigeon.png" alt="Rock Dove Comic"/>
+    </section>
+  </div>
+`;
