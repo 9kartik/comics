824110c2d968f8cd8d8ed119566e91fce2818927
9kartik <mauryakartik@gmail.com>
2024-09-17 02:04:54 +0530

    switching to npm for build consistency

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,31 @@ import "./style.css";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="slider">
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Cover.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Cover.png" alt="Cover"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Chargeable.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Chargeable.png" alt="Chargeable"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Child_power.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Child_power.png" alt="Children power"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Compliment.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Compliment.png" alt="Compliment"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Drivers_license.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Drivers_license.png" alt="Drivers license"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Ex_wedding.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Ex_wedding.png" alt="Wedding of the ex"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Lizard.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Lizard.png" alt="Lizard Lizard on the wall"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Marriage.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Marriage.png" alt="Marriage"/>
     </section>
     <section>  
-      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Nails.png" alt="Rock Dove Comic"/>
+      <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Nails.png" alt="Nails growth"/>
     </section>
     <section>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Rock_pigeon.png" alt="Rock Dove Comic"/>
