622bff45473ac80bfa50f42f1c0a910a897f4f4a
9kartik <mauryakartik@gmail.com>
2024-09-17 21:59:24 +0530

    turned to list; no background color

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,42 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="slider">
-    <section>  
+  <ol class="slider">
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Cover.png" alt="Cover"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Rock_pigeon.png" alt="Rock Dove Comic"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Compliment.png" alt="Compliment"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Drivers_license.png" alt="Drivers license"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Lizard.png" alt="Lizard Lizard on the wall"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Chargeable.png" alt="Chargeable"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Nails.png" alt="Nails growth"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
     <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Ex_wedding.png" alt="Wedding of the ex"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
     <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Marriage.png" alt="Marriage"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Child_power.png" alt="Children power"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <img loading="lazy" src="https://some-imgs.s3.ap-south-1.amazonaws.com/comics/Runner_nature.png" alt="Rock Dove Comic"/>
-    </section>
-    <section>  
+    </li>
+    <li>  
       <h1>More coming soon...</h1>
-    </section>
-  </div>
+    </li>
+  </ol>
 `;
